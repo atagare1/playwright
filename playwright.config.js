95d6185b842f6cc10a719edb665e92692b1d7e43
Aarya Sandip Tagare <aarya.tagare@happiestminds.com>
2024-06-26 20:00:05 +0530

    removed forbid only for testing purpose

--- playwright.config.js
+++ playwright.config.js
@@ -15,7 +15,7 @@ module.exports = defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+ // forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
@@ -30,9 +30,9 @@ module.exports = defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     //trace: 'on-first-retry',
     browserName:'chromium',
-    headless: false,
     screenshot: 'on',
     trace:'retain-on-failure',
+    video:'retain-on-failure'
   },
 
   
